793333341eda67555308ced55adf87ffb81295d6
Rikhardson <n2puri00@students.oamk.fi>
2024-03-19 14:04:40 +0200

    EDU-1037 Image added to bottom of representatives screen
    Change-Id: I845d4f7cf64b5a24762f0fcb9b8d64428e228350

--- views/representatives.js
+++ views/representatives.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { ScrollView } from "react-native";
+import { Image, ScrollView, View } from "react-native";
 import { Text, List, TextInput } from "react-native-paper";
 import Representative from "./representative";
 
@@ -26,32 +26,40 @@ export default Representatives = () => {
 
   useEffect(() => {
     axios.get("https://avoindata.eduskunta.fi/api/v1/seating/")
-    .then((response) => {
-      setSeatings(response.data)
+      .then((response) => {
+        setSeatings(response.data)
         setFilteredSeatings(response.data)
-    })
+      })
   }, [])
-  
+
   return (
     <ScrollView>
       <TextInput
         placeholder="Hae kansanedustajaa"
         value={searchQuery}
         onChangeText={handleSearch}
-        />
-        <List.Section>
+      />
+      <List.Section>
         {filteredSeatings.map((seat, index) => (
           <List.Item
             key={index.toString()}
             title={<Text>{seat.firstname} {seat.lastname}</Text>}
             description={<Text>{seat.party}</Text>}
-            
-              // fix
-              // onPress={<Representative/>}
-              >
-            </List.Item>
+
+          // fix
+          // onPress={<Representative/>}
+          >
+          </List.Item>
         ))}
       </List.Section>
+      {/* Image at the bottom covering the whole width */}
+      <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+        <Image
+          source={require('../images/eduskunta11.png')} // Replace with your image path
+          style={{ width: '100%', height: 150 }}
+          resizeMode="stretch"
+        />
+      </View>
     </ScrollView>
   );
 };
