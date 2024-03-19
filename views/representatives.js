ae43e854c90dbe81e7e4ddca7b83828a88aa6dee
Ossi Juvani <n0juos00@students.oamk.fi>
2024-03-19 12:53:15 +0200

    EDU-1035 Create search bar to filter representatives
    Change-Id: I666f80dab9a9d8861a4069f9a7c1b62095aeda65

--- views/representatives.js
+++ views/representatives.js
@@ -1,38 +1,55 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { ScrollView } from "react-native";
-import { Text, List } from "react-native-paper";
+import { Text, List, TextInput } from "react-native-paper";
 import Representative from "./representative";
 
 
 export default Representatives = () => {
-
   const [seatings, setSeatings] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [filteredSeatings, setFilteredSeatings] = useState([]);
+
+  useEffect(() => {
+
+  }, []);
+
+  const handleSearch = (query) => {
+    setSearchQuery(query);
+    const filteredData = seatings.filter(seat =>
+      seat.firstname.toLowerCase().includes(query.toLowerCase()) ||
+      seat.lastname.toLowerCase().includes(query.toLowerCase()) ||
+      seat.party.toLowerCase().includes(query.toLowerCase())
+    );
+    setFilteredSeatings(filteredData);
+  };
 
   useEffect(() => {
     axios.get("https://avoindata.eduskunta.fi/api/v1/seating/")
     .then((response) => {
       setSeatings(response.data)
+        setFilteredSeatings(response.data)
     })
   }, [])
   
   return (
     <ScrollView>
-      <List.Section>
-        {seatings.map((seat, index) => (
+      <TextInput
+        placeholder="Hae kansanedustajaa"
+        value={searchQuery}
+        onChangeText={handleSearch}
+        />
+        <List.Section>
+        {filteredSeatings.map((seat, index) => (
           <List.Item
             key={index.toString()}
-            title={
-              <Text>{seat.firstname} {seat.lastname}</Text>
-            }
-            description={
-              <Text>{seat.party}</Text>
-            }
-            // fix
-            // onPress={<Representative/>}
+            title={<Text>{seat.firstname} {seat.lastname}</Text>}
+            description={<Text>{seat.party}</Text>}
             
-            >
-          </List.Item>
+              // fix
+              // onPress={<Representative/>}
+              >
+            </List.Item>
         ))}
       </List.Section>
     </ScrollView>
