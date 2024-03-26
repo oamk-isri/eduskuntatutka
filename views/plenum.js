e7ba5c0da615e618bbacc66a5a4f6dfcd39188aa
Rikhardson <n2puri00@students.oamk.fi>
2024-03-26 13:49:05 +0200

    EDU-1090 Create a page which show the video and details of a plenum
    Change-Id: I02f3197afba3172a5a32dadcd321f809bc3410a5

---
+++ views/plenum.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { View, Text } from "react-native";
+import { WebView } from 'react-native-webview';
+
+export default PlenumDetails = ({ route }) => {
+  const { event } = route.params;
+  const { title, urlName } = event;
+
+  // Construct the complete video URL
+  const videoUrl = `https://eduskunta.videosync.fi/${urlName}?embed-view=1`;
+
+  return (
+    <View style={{ flex: 1 }}>
+      <Text style={{ fontSize: 20, fontWeight: 'bold', textAlign: 'center', marginVertical: 20 }}>{title}</Text>
+      <WebView
+        source={{ uri: videoUrl }}
+        style={{ flex: 1 }}
+      />
+    </View>
+  );
+};
