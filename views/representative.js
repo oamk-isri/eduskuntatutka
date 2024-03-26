159c88f73dc648a1a1d4caad80d8ddda28497c48
oamk-isri <n2isri00@students.oamk.fi>
2024-03-26 11:07:38 +0200

    EDU-1082 Implement profile images for representatives in profile view
    Change-Id: Iab4247b95df0d61295f6e27b23dedb9bc510af09

--- views/representative.js
+++ views/representative.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 import { useEffect, useState } from "react"
-import { View, Text } from "react-native";
+import { StyleSheet, View, Text, Image } from "react-native";
 
-export default Representative = ({route}) => {
+export default Representative = ({ route }) => {
 
   const { id, image } = route.params
 
@@ -14,12 +14,12 @@ export default Representative = ({route}) => {
   const link =
     `https://avoindata.eduskunta.fi/api/v1/memberofparliament/${hetekaId}/fi`
 
-  
+
   useEffect(() => {
     axios.get(link)
-    .then((response) => {
-      populateState(response.data.jsonNode)
-    })
+      .then((response) => {
+        populateState(response.data.jsonNode)
+      })
   }, [])
 
   const populateState = (data) => {
@@ -29,8 +29,36 @@ export default Representative = ({route}) => {
 
   return (
     <View>
-      <Text>This is the representative view!</Text>
-      <Text>This page is {firstname} {lastname}'s. {pictureUrl}</Text>
+      <View styles={styles.container}>
+        <Image
+          source={{ uri: pictureUrl }}
+          style={styles.image}
+        />
+        <Text
+          style={styles.titleName}>
+          {firstname} {lastname}
+        </Text>
+      </View>
+
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  image: {
+    height: 200,
+    width: 200,
+    borderRadius: 100,
+    alignSelf: "center",
+  },
+  titleName: {
+    fontSize: 25,
+    paddingTop: 15,
+    alignSelf: "center"
+  }
+});
