d435fc01b61ac1776d290d8ed27eb1cdea0d63c0
oamk-isri <n2isri00@students.oamk.fi>
2024-03-26 12:00:01 +0200

    EDU-1087 Implement political party color schemes definition
    Change-Id: I30c99487cc59f900a6a0a4aae4eaaf7479868dd8

---
+++ styles/parties.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  // sorted alphabetically
+  kd: {color: "rgb(255, 147, 4)"},
+  kesk: {color: "rgb(0, 107, 106)"},
+  kok: {color: "rgb(0, 92, 169)"},
+  liik: {color: "rgb(174, 35, 117)"},
+  ps: {color: "rgb(255, 213, 0)"},
+  r: {color: "rgb(255, 251, 218)"},
+  sd: {color: "rgb(225, 25, 49)"},
+  vas: {color: "rgb(255, 120, 200)"},
+  vihr: {color: "rgb(118, 183, 42)"},
+});
