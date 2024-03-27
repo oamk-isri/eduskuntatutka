f078e6a0cd66c93a61d01ce52a52643b01c06bfa
oamk-isri <n2isri00@students.oamk.fi>
2024-03-27 12:17:26 +0200

    EDU-1093 Fix main Appbar visibility
    Change-Id: Ia881524cbb09644eafeafdc292ca7e61013dc287

--- components/RootNavigator.js
+++ components/RootNavigator.js
@@ -3,16 +3,12 @@ import { Image } from 'react-native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { Appbar, Drawer } from 'react-native-paper';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import PlenumList from '../views/plenumList'; // Import the PlenumList component
-import Representatives from '../views/representatives'; // Import the Representatives component
-// import PlenumStack from './PlenumStack';
-// import RepStack from './RepStack';
 import { PlenumsStack, RepresentativesStack } from './StackNavigators';
 import Info from '../views/info';
 
 const MainDrawer = createDrawerNavigator();
 
-function DrawerView({ navigation }) {
+const DrawerView = ({ navigation }) => {
   return (
     <SafeAreaView>
       <Drawer.Section title="Some title">
@@ -33,6 +29,16 @@ function DrawerView({ navigation }) {
   );
 }
 
+const MainHeader = ({ navigation }) => {
+  return (
+    <Appbar.Header style={{ height: 75 }}>
+      <Appbar.Action icon="menu" onPress={() => navigation.toggleDrawer()} />
+      <Appbar.Content title="Eduskuntatutka" />
+      <Image source={require('../images/tutkalogo.png')} style={{ width: 70, height: 70, marginEnd: 10 }} />
+    </Appbar.Header>
+  )
+}
+
 export default RootNavigator = () => {
   return (
     <MainDrawer.Navigator
@@ -42,29 +48,29 @@ export default RootNavigator = () => {
         name="MainView"
         component={RepresentativesStack}
         options={{
-          header: ({ navigation }) => (
-            <Appbar.Header style={{ height: 75 }}>
-              <Appbar.Action icon="menu" onPress={() => navigation.toggleDrawer()} />
-              <Appbar.Content title="Eduskuntatutka" />
-              <Image source={require('../images/tutkalogo.png')} style={{ width: 70, height: 70, marginEnd: 10 }} />
-            </Appbar.Header>
-          )
+          header: ({ navigation }) => <MainHeader navigation={navigation} />
         }}
       />
       <MainDrawer.Screen
         name="PlenumsStack"
         component={PlenumsStack}
-        options={{drawerLabel: "Plenum List"}}
+        options={{
+          header: ({ navigation }) => <MainHeader navigation={navigation} />
+        }}
       />
       <MainDrawer.Screen
         name="RepresentativesStack"
         component={RepresentativesStack}
-        options={{drawerLabel: "Representatives"}}
+        options={{
+          header: ({ navigation }) => <MainHeader navigation={navigation} />
+        }}
       />
       <MainDrawer.Screen
         name="Info"
         component={Info}
-        options={{ drawerLabel: 'Info'}}
+        options={{
+          header: ({ navigation }) => <MainHeader navigation={navigation} />
+        }}
       />
     </MainDrawer.Navigator>
   );
