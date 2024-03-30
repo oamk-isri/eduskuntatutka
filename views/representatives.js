21b0dcf6efe854c3ffa9fb3cd2be80c9131e5f63
oamk-isri <n2isri00@students.oamk.fi>
2024-03-30 16:47:47 +0200

    EDU-1109 Add activity indicator to representatives view when loading
    Change-Id: Ia00e73744d5a6b8c97d217a6d9812497be5e3f21

--- views/representatives.js
+++ views/representatives.js
@@ -1,12 +1,13 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { Image, ScrollView, View } from "react-native";
-import { Text, List, TextInput } from "react-native-paper";
+import { Text, List, TextInput, ActivityIndicator } from "react-native-paper";
 import Representative from "./representative";
 
 
-export default Representatives = ({navigation}) => {
+export default Representatives = ({ navigation }) => {
 
+  const [isLoading, setIsLoading] = useState(true);
   const [seatings, setSeatings] = useState([]);
   const [searchQuery, setSearchQuery] = useState('');
   const [filteredSeatings, setFilteredSeatings] = useState([]);
@@ -19,6 +20,7 @@ export default Representatives = ({navigation}) => {
       seat.party.toLowerCase().includes(query.toLowerCase())
     );
     setFilteredSeatings(filteredData);
+    
   };
 
   useEffect(() => {
@@ -26,6 +28,7 @@ export default Representatives = ({navigation}) => {
       .then((response) => {
         setSeatings(response.data)
         setFilteredSeatings(response.data)
+        setIsLoading(false);
       })
   }, [])
 
@@ -36,27 +39,30 @@ export default Representatives = ({navigation}) => {
         value={searchQuery}
         onChangeText={handleSearch}
       />
-      <List.Section>
-        {filteredSeatings.map((seat, index) => (
-          <List.Item
-            key={index.toString()}
-            title={<Text>{seat.firstname} {seat.lastname}</Text>}
-            description={<Text>{seat.party}</Text>}
+      {isLoading ? (
+        <ActivityIndicator color="red" />
+      ) : (
+        <List.Section>
+          {filteredSeatings.map((seat, index) => (
+            <List.Item
+              key={index.toString()}
+              title={<Text>{seat.firstname} {seat.lastname}</Text>}
+              description={<Text>{seat.party}</Text>}
 
-          onPress={() => navigation.navigate(
-            "Representative",
-            {
-              id: seat.hetekaId,
-              image: seat.pictureUrl
-            })}
-          >
-          </List.Item>
-        ))}
-      </List.Section>
-      {/* Image at the bottom covering the whole width */}
+              onPress={() => navigation.navigate(
+                "Representative",
+                {
+                  id: seat.hetekaId,
+                  image: seat.pictureUrl
+                })}
+            >
+            </List.Item>
+          ))}
+        </List.Section>
+      )}
       <View style={{ alignItems: 'center', justifyContent: 'center' }}>
         <Image
-          source={require('../images/eduskuntatalo.png')} // Replace with your image path
+          source={require('../images/eduskuntatalo.png')}
           style={{ width: '100%', height: 120 }}
           resizeMode="stretch"
         />
