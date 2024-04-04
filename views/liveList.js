8fdf893356a54cba254f37b230473f4fa42475df
Rikhardson <n2puri00@students.oamk.fi>
2024-04-04 14:34:59 +0300

    EDU-1125 Live-stream's page added
    Change-Id: I044b68b45fb00afdd0aa57c7a40bad7895a93e32

---
+++ views/liveList.js
@@ -0,0 +1,63 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Image, ScrollView, View, TouchableOpacity, Button } from "react-native";
+import { Text, Card } from "react-native-paper";
+
+export default liveList = ({ navigation }) => {
+  const [events, setEvents] = useState([]);
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    fetchEvents();
+  }, []);
+
+  const fetchEvents = () => {
+    axios
+      .get(
+        `https://verkkolahetys.eduskunta.fi/api/v1/categories/slug/eduskunta-kanava?include=children,events&states=0&limit=8&page=${page}`
+      )
+      .then((response) => {
+        if (response.data && response.data.events) {
+          setEvents([...events, ...response.data.events.map(event => {
+            // Split the title at the '|' mark and take the first part
+            const title = event.title.split('|')[0].trim();
+            return { ...event, title };
+          })]);
+          setPage(page + 1);
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  };
+
+  return (
+    <ScrollView>
+      {events.length === 0 && (
+        <Text style={{ fontSize: 18, fontWeight: 'bold', textAlign: 'center', marginVertical: 10 }}>
+          Suoria lähetyksiä ei ole käynnissä.
+        </Text>
+      )}
+      {events.map((event) => (
+        <TouchableOpacity key={event._id} onPress={() => navigation.navigate("Suorat lähetykset", { event })}>
+          <Card style={{margin:5}}>
+            <Card.Cover source={{ uri: `https://eduskunta.videosync.fi${event.previewImg}` }} />
+            <Card.Content>
+              <Text style={{fontSize: 18, fontWeight: 'bold', paddingTop: 10}}>{event.title}</Text>
+            </Card.Content>
+          </Card>
+        </TouchableOpacity>
+      ))}
+      <View style={{ alignItems: "center", justifyContent: "center", marginVertical: 10 }}>
+        <Button title="Näytä lisää" onPress={fetchEvents} />
+      </View>
+      <View style={{ alignItems: "center", justifyContent: "center" }}>
+        <Image
+          source={require("../images/eduskuntatalo.png")}
+          style={{ width: "100%", height: 120 }}
+          resizeMode="stretch"
+        />
+      </View>
+    </ScrollView>
+  );
+};
