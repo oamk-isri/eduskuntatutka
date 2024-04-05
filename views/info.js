d99cc72afe2d7bbe630033774c560b73d636e219
Ossi Juvani <n0juos00@students.oamk.fi>
2024-04-05 13:34:30 +0300

    EDU-1130 Add dropdowns and authors to info page
    Change-Id: Ie170a86b110155a9c705fb85712c592676331785

--- views/info.js
+++ views/info.js
@@ -1,52 +1,86 @@
-import React from "react";
-import { Text, View, Image, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { Text, View, Image, StyleSheet, TouchableOpacity } from "react-native";
 
 
 export default Info = () => {
-  
-    return (
-      <View style={styles.container}>
-        <Text
-            style={styles.title}>
-            Tietoa sovelluksesta
-        </Text>
-          <View style={styles.textContainer}>
-            <Text
-              style={styles.text}>
+
+  const [showInfo, setShowInfo] = useState(false);
+  const [showAuthors, setShowAuthors] = useState(false);
+
+  const toggleInfo = () => {
+    setShowInfo(!showInfo);
+    setShowAuthors(false);
+  };
+
+  const toggleAuthors = () => {
+    setShowAuthors(!showAuthors);
+    setShowInfo(false);
+  };
+
+  return (
+    <View>
+      <TouchableOpacity onPress={toggleInfo}>
+        <View style={styles.titleContainer}>
+          <Text style={styles.title}>Tietoa sovelluksesta</Text>
+          <Text style={styles.dropdownIcon}>{showInfo ? '▲' : '▼'}</Text>
+        </View>
+      </TouchableOpacity>
+      {showInfo && (
+        <View style={styles.infoContainer}>
+          <View>
+            <Text style={styles.text}>
               Eduskuntatutka tarjoaa käyttäjilleen helpon ja kätevän tavan pysyä ajantasalla Suomen eduskunnan toiminnasta.
-              Sovelluksen avulla voit tutustua kansanedustajiin ja heidän taustoihinsa, seurata täysistuntoja ja saada tietoa ajankohtaisista poliittisista tapahtumista.            </Text>
-            <Text
-              style={styles.text}>
+              Sovelluksen avulla voit tutustua kansanedustajiin ja heidän taustoihinsa, seurata täysistuntoja ja saada tietoa ajankohtaisista poliittisista tapahtumista.
+            </Text>
+            <Text style={styles.text}>
               Sovellus hyödyntää eduskunnan avointa dataa, mikä tarkoittaa, että kaikki tieto on luotettavaa ja ajantasalla.
             </Text>
           </View>
-        <View style={styles.imageContainer}>
-          <Image
-            source={require('../images/eduskuntatalo.png')} // Replace with your image path
-            style={styles.image}
-            resizeMode="stretch"
-          />
         </View>
-      </View>
-    );
-  };
+      )}
+       <TouchableOpacity onPress={toggleAuthors}>
+        <View style={styles.titleContainer}>
+          <Text style={styles.title}>Tekijät</Text>
+          <Text style={styles.dropdownIcon}>{showAuthors ? '▲' : '▼'}</Text>
+        </View>
+      </TouchableOpacity>
+      {showAuthors && (
+        <View>
+          <Text style={styles.author}>Aleksi Kallio</Text>
+          <Text style={styles.author}>Riku Puonti</Text>
+          <Text style={styles.author}>Risto-Matti Isola</Text>
+          <Text style={styles.author}>Ossi Juvani</Text>          
+        </View>
+      )}
+    </View>
+  );
+};
 
-  
 const styles = StyleSheet.create({
+  titleContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+    paddingVertical: 15,
+    borderBottomWidth: 1,
+  },
   title: {
+    flex: 1,
     fontSize: 24,
     fontWeight: "bold",
-    padding: 20
   },
-  textContainer: {
-    marginBottom: 20,
+  dropdownIcon: {
+    fontSize: 20,
+  },
+  infoContainer: {
+    marginHorizontal: 20,
   },
   text: {
     fontSize: 18,
+    paddingVertical: 15,
+  },
+  author: {
+    fontSize: 18,
     padding: 15
-  },
-  image: {
-    width: "100%",
-    height: 120,
-  },
+  }
 });
