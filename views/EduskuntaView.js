d30b8b99220afedbd5dfa9e065178d88df15a878
Ossi Juvani <n0juos00@students.oamk.fi>
2024-04-11 15:20:33 +0300

    EDU-1146 Add images and text to Eduskunta view
    Change-Id: Iacaf746c3067a87dc377770cb5279e7165bea5b2

--- views/EduskuntaView.js
+++ views/EduskuntaView.js
@@ -12,18 +12,42 @@ const EduskuntaScreen = () => (
       style={{ width: '100%', height: 150 }}
       resizeMode="stretch"
     /> 
+    <Text style={{fontSize: 18, margin: 10}}> 
+      Suomen eduskunta on maamme lainsäädäntöelin, joka vastaa Suomen yhteiskunnan päätöksenteosta.
+      Se koostuu 200 kansanedustajasta, jotka valitaan neljän vuoden välein vaaleilla.
+    </Text> 
+
+    <Text style={{fontSize: 18, margin: 10}}> 
+      Eduskunta käsittelee lakeja ja säätää niitä, hyväksyy valtion budjetin ja valvoo hallituksen toimintaa.
+    </Text> 
+      
   </View>
 );
 
 const ToimintaScreen = () => (
   <View>
     <Text style={{ fontSize: 24, padding: 10 }}>Toiminta</Text>
+    <Image 
+      source={require('../images/eduskunta1.png')} // toinen kuva tähän
+      style={{ width: '100%', height: 150 }} 
+      resizeMode="stretch"
+    /> 
+    <Text style={{fontSize: 18, margin: 10}}>
+      Eduskunta kokoontuu täysistuntoihin, joissa käsitellään lakiesityksiä, tehdään päätöksiä ja keskustellaan ajankohtaisista aiheista.
+      Lisäksi eduskunnassa toimii erilaisia valiokuntia, jotka valmistelevat lainsäädäntöä ja käsittelevät erityiskysymyksiä. 
+      Eduskunnan jäsenet osallistuvat myös erilaisiin kansainvälisiin tehtäviin ja vierailuihin
+    </Text>
   </View>
 );
 
 const EduskuntaryhmatScreen = () => (
   <View>
     <Text style={{ fontSize: 24, padding: 10 }}>Eduskuntaryhmät</Text> 
+    <Image 
+      source={require('../images/eduskunta1.png')} // toinen kuva tähän
+      style={{ width: '100%', height: 150 }} 
+      resizeMode="stretch"
+    /> 
   </View>
 );
 
