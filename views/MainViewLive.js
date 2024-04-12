2b8a1846d7648b5ba62da0db243961cfab4b2678
al-lu <devaaja@hotmail.com>
2024-04-12 17:53:09 +0300

    EDU-1149 Display todays events along with upcomin events in MainView
    - Add missing dependencies
    
    Change-Id: I67d12cf353d97bd032cd08a4871762e980af6f44

--- views/MainViewLive.js
+++ views/MainViewLive.js
@@ -1,68 +1,17 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { View, TouchableOpacity } from "react-native";
 import { Text, Card } from "react-native-paper";
+import { isSameDay, parseISO, isAfter } from "date-fns";
+import Heading from "../components/Heading";
 
-export default MainViewLive = ({ navigation }) => {
-  const [event, setEvent] = useState(null);
-  const [page, setPage] = useState(1);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const taysistuntoResponse = await axios.get(
-          `https://verkkolahetys.eduskunta.fi/api/v1/categories/slug/taysistunnot?include=events&limit=1&page=${page}`
-        );
-        const seminaaritResponse = await axios.get(
-          `https://verkkolahetys.eduskunta.fi/api/v1/categories/slug/seminaarit?include=events&limit=1&page=${page}`
-        );
-
-        const taysistuntoEvents = taysistuntoResponse.data.events.flat();
-        const seminaaritEvents = seminaaritResponse.data.events.flat();
-
-        const upcomingTaysistunto = taysistuntoEvents.find((event) =>
-          isUpcoming(event.publishingDate)
-        );
-        const upcomingSeminaari = seminaaritEvents.find((event) =>
-          isUpcoming(event.publishingDate)
-        );
-
-        let upcomingEvent;
-        if (upcomingTaysistunto) {
-          upcomingEvent = upcomingTaysistunto;
-        } else if (upcomingSeminaari) {
-          upcomingEvent = upcomingSeminaari;
-        }
-
-        setEvent(upcomingEvent);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
-
-    fetchData();
-  }, []);
-
-  const isUpcoming = (publishingDate) => {
-    const parsedDate = new Date(publishingDate);
-
-    const today = new Date();
-
-    return parsedDate > today;
-  };
-
-  const handlePressEvent = (event) => {
-    const { urlName } = event;
-    if (urlName.includes("taysistunto")) {
-      navigation.navigate("PlenumDetails", { taysistunnotEvent: event });
-    } else {
-      navigation.navigate("Suora lähetys", { liveEvent: event });
-    }
-  };
-
+const TodayEvents = ({ events, handlePressEvent }) => {
   return (
-    <View>
-      {event ? (
+    <>
+      <Heading size="h3" style={{ paddingBottom: 20 }}>
+        Lähetykset tänään
+      </Heading>
+      {events.map((event) => (
         <TouchableOpacity
           key={event._id}
           onPress={() => handlePressEvent(event)}
@@ -77,21 +26,128 @@ export default MainViewLive = ({ navigation }) => {
               <Text
                 style={{ fontSize: 18, fontWeight: "bold", paddingTop: 10 }}
               >
-                Seuraava {event.title.split("|")[0].trim().toLowerCase()}
+                Tänään: {event.title.split("|")[0].trim().toLowerCase()}
               </Text>
             </Card.Content>
           </Card>
         </TouchableOpacity>
-      ) : (
-        <Text
-          style={{
-            fontSize: 18,
-            fontWeight: "bold",
-            textAlign: "center",
-            marginVertical: 10,
-          }}
+      ))}
+    </>
+  );
+};
+
+const UpcomingEvents = ({ events, handlePressEvent }) => {
+  return (
+    <>
+      <Heading size="h3" style={{ paddingBottom: 20 }}>
+        Tulevat lähetykset
+      </Heading>
+      {events.map((event) => (
+        <TouchableOpacity
+          key={event._id}
+          onPress={() => handlePressEvent(event)}
         >
-          Ei suoria lähetyksiä juuri nyt.
+          <Card style={{ margin: 5 }}>
+            <Card.Cover
+              source={{
+                uri: `https://eduskunta.videosync.fi${event.previewImg}`,
+              }}
+            />
+            <Card.Content>
+              <Text
+                style={{ fontSize: 18, fontWeight: "bold", paddingTop: 10 }}
+              >
+                {event.title.split("|")[0].trim().toLowerCase()}
+              </Text>
+            </Card.Content>
+          </Card>
+        </TouchableOpacity>
+      ))}
+    </>
+  );
+};
+
+export default MainViewLive = ({ navigation }) => {
+  const [eventsToday, setEventsToday] = useState([]);
+  const [eventsUpcoming, setEventsUpcoming] = useState([]);
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const taysistuntoResponse = await axios.get(
+          `https://verkkolahetys.eduskunta.fi/api/v1/categories/slug/taysistunnot?include=events&limit=1&page=1`
+        );
+        const seminaaritResponse = await axios.get(
+          `https://verkkolahetys.eduskunta.fi/api/v1/categories/slug/seminaarit?include=events&limit=1&page=1`
+        );
+        const valiokunnatResponse = await axios.get(
+          `https://verkkolahetys.eduskunta.fi/api/v1/categories/slug/valiokuntien-julkiset-kuulemiset-ja-avoimet-kokoukset?include=events&limit=1&page=1`
+        );
+        const tiedotustilaisuudetResponse = await axios.get(
+          `https://verkkolahetys.eduskunta.fi/api/v1/categories/slug/tiedotustilaisuudet?include=events&states=1&limit=1&page=1`
+        );
+        const esittelyResponse = await axios.get(
+          `https://verkkolahetys.eduskunta.fi/api/v1/categories/slug/esittelyvideot?include=events&states=1&limit=1&page=1`
+        );
+        const eduskuntaryhmatResponse = await axios.get(
+          `https://verkkolahetys.eduskunta.fi/api/v1/categories/slug/eduskuntaryhmat?include=events&states=1&limit=1&page=1`
+        );
+
+        const taysistuntoEvents = taysistuntoResponse.data.events.flat();
+        const seminaaritEvents = seminaaritResponse.data.events.flat();
+        const valiokunnatEvents = valiokunnatResponse.data.events.flat();
+        const tiedotustilaisuudetEvents =
+          tiedotustilaisuudetResponse.data.events.flat();
+        const esittelyEvents = esittelyResponse.data.events.flat();
+        const eduskuntaryhmatEvents =
+          eduskuntaryhmatResponse.data.events.flat();
+
+        const allEvents = [
+          ...taysistuntoEvents,
+          ...seminaaritEvents,
+          ...valiokunnatEvents,
+          ...tiedotustilaisuudetEvents,
+          ...esittelyEvents,
+          ...eduskuntaryhmatEvents,
+        ];
+
+        const todaysEvents = allEvents.filter((event) =>
+          isSameDay(parseISO(event.publishingDate), new Date())
+        );
+
+        const upcomingEvents = allEvents.filter((event) =>
+          isAfter(parseISO(event.publishingDate), new Date())
+        );
+
+        setEventsToday(todaysEvents);
+        setEventsUpcoming(upcomingEvents);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const handlePressEvent = (event) => {
+    navigation.navigate("Suora lähetys", { liveEvent: event });
+  };
+
+  return (
+    <View>
+      {eventsToday.length > 0 && (
+        <TodayEvents events={eventsToday} handlePressEvent={handlePressEvent} />
+      )}
+      {eventsUpcoming.length > 0 && (
+        <UpcomingEvents
+          events={eventsUpcoming}
+          handlePressEvent={handlePressEvent}
+        />
+      )}
+      {eventsToday.length === 0 && eventsUpcoming.length === 0 && (
+        <Text style={{ textAlign: "center", marginVertical: 10 }}>
+          Ei suoria lähetyksiä tai tulevia tapahtumia.
         </Text>
       )}
     </View>
