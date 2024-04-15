ff2503efa0fcb4175da369f2706a9245c0a059e7
oamk-isri <n2isri00@students.oamk.fi>
2024-04-15 15:46:31 +0300

    EDU-1160 Party name and color for representative view
    Change-Id: Ic35b6c7c496087608fc5035428271406ec398e74

--- views/representative.js
+++ views/representative.js
@@ -5,12 +5,15 @@ import { ActivityIndicator } from "react-native-paper";
 import Absences from "../components/AbsenceParser";
 import Speeches from "../components/repSpeeches";
 import Details from "../components/repDetails";
+import parties from "../styles/parties";
 
 export default Representative = ({ route }) => {
 
-  const { id, image } = route.params
+  const { id, image, party } = route.params
 
   const [hetekaId, setHetekaId] = useState(id)
+  const [shortParty, setShortParty] = useState(party)
+  const [longParty, setLongParty] = useState("")
   const [firstname, setFirstname] = useState("");
   const [lastname, setLastname] = useState("");
   const [pictureUrl, setPictureUrl] = useState("https://avoindata.eduskunta.fi/" + image);
@@ -33,6 +36,7 @@ export default Representative = ({ route }) => {
     setFirstname(data.Henkilo.KutsumaNimi);
     setLastname(data.Henkilo.SukuNimi)
     setData(data.Henkilo)
+    setLongParty(data.Henkilo.Eduskuntaryhmat.NykyinenEduskuntaryhma.Nimi)
   }
 
   return (
@@ -41,14 +45,16 @@ export default Representative = ({ route }) => {
       <View styles={styles.container}>
         <Image
           source={{ uri: pictureUrl }}
-          style={styles.image}
+          style={[styles.image, styles[shortParty]]}
         />
         <Text
           style={styles.titleName}>
           {firstname} {lastname}
         </Text>
+        <Text
+          style={styles.longParty}>{longParty}</Text>
       </View>
-      <Details data={data} />
+      <Details data={data} longParty={longParty} />
       <Speeches id={hetekaId} />
       {isLoading ? (
         <ActivityIndicator />
@@ -72,10 +78,27 @@ const styles = StyleSheet.create({
     width: 200,
     borderRadius: 100,
     alignSelf: "center",
+    borderWidth: 5
   },
   titleName: {
     fontSize: 25,
     paddingTop: 15,
+    paddingBottom: 3,
     alignSelf: "center"
-  }
+  },
+  longParty: {
+    fontSize: 18,
+    flexWrap: "wrap",
+    alignSelf: "center",
+    paddingBottom: 15
+  },
+  kd: {borderColor: "rgb(255, 147, 4)"},
+  kesk: {borderColor: "rgb(0, 107, 106)"},
+  kok: {borderColor: "rgb(0, 92, 169)"},
+  liik: {borderColor: "rgb(174, 35, 117)"},
+  ps: {borderColor: "rgb(255, 213, 0)"},
+  r: {borderColor: "rgb(255, 251, 218)"},
+  sd: {borderColor: "rgb(225, 25, 49)"},
+  vas: {borderColor: "rgb(255, 120, 200)"},
+  vihr: {borderColor: "rgb(118, 183, 42)"},
 });
