045c830b3b48225203be5c3dc8b785b7f025bf6a
oamk-isri <n2isri00@students.oamk.fi>
2024-04-23 14:45:09 +0300

    EDU-1230 Create boilerplate files for themes
    Change-Id: I729b35b0734d59eea4ac954a658b7d0104e29a3b

---
+++ styles/views/broadcasts.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+
+  // separate component specific definitions with commments 
+
+  example: {
+    fontSize: 20,
+  },
+});
