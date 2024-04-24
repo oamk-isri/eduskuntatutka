03b6d0a0a990902eb5c03e1c6d2dceb82ed009d4
oamk-isri <n2isri00@students.oamk.fi>
2024-04-24 17:31:29 +0300

    EDU-1239 Refactor newsFeed & parsers to use external theming
    Change-Id: Iaa8d28bf3fbe63f13b2fed441249bd8793fe28da

--- components/newsfeed/News.js
+++ components/newsfeed/News.js
@@ -1,10 +1,11 @@
 import { useEffect } from "react";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 import { Divider } from "react-native-paper";
 import { RssFeeds } from "../../constants/RssFeeds";
 import { FlatList } from "react-native-gesture-handler";
 import FinnishDate from "../parsers/FinnishDate";
 import Xml from "../parsers/Xml";
+import styles from "../../styles/components/newsfeed"
 
 export default News = ({ navigation, route }) => {
   const { getData, data } = Xml();
@@ -34,10 +35,10 @@ export default News = ({ navigation, route }) => {
   const NewsItem = ({ item }) => (
     <TouchableOpacity onPress={() => handlePress(item.link)}>
       <View style={styles.newsContainer}>
-        <View style={{ flex: 0.2 }}>
+        <View style={styles.newsLeft}>
           <FinnishDate dateString={item.pubDate} />
         </View>
-        <View style={{ flex: 0.8 }}>
+        <View style={styles.newsRight}>
           {item.title.length > 40 || !item.description.length ? (
             <Text style={styles.title}>{item.title}</Text>
           ) : (
@@ -60,38 +61,3 @@ export default News = ({ navigation, route }) => {
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  tagContainer: {
-    padding: 15,
-    flexDirection: "row",
-    justifyContent: "space-between",
-    overflow: "scroll",
-  },
-
-  newsContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    marginLeft: 10,
-    marginRight: 10,
-  },
-  title: {
-    flex: 0.7,
-    fontSize: 14,
-    paddingBottom: 20,
-    paddingTop: 20,
-  },
-  headingContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-  },
-  buttonRight: {
-    marginLeft: "auto",
-    marginRight: 30,
-    marginTop: 10,
-  },
-  buttonText: {
-    fontWeight: "bold",
-    fontSize: 25,
-  },
-});
--- styles/components/newsfeed.js
+++ styles/components/newsfeed.js
@@ -2,7 +2,67 @@ import { StyleSheet } from 'react-native';
 
 export default styles = StyleSheet.create({
   
-  example: {
-    fontSize: 20,
+  // News.js & NewsFeed.js
+  
+  tagContainer: {
+    padding: 15,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    overflow: "scroll",
   },
+  newsContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  newsLeft: {
+    flex: 0.2,
+    paddingTop: 10,
+    paddingBottom: 10,
+  },
+  newsRight: {
+    flex: 0.8,
+  },
+  title: {
+    flex: 0.7,
+    fontSize: 14,
+    paddingBottom: 20,
+    paddingTop: 20,
+  },
+  headingContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  buttonRight: {
+    marginLeft: "auto",
+    marginRight: 30,
+    marginTop: 20,
+  },
+  buttonText: {
+    fontWeight: "bold",
+    fontSize: 15,
+  },
+
+  // NewsTags.js
+
+  tag: {
+    backgroundColor: "#f2f2f2",
+    padding: 10,
+    marginHorizontal: 5,
+  },
+  activeTag: {
+    borderBottomWidth: 3,
+    borderColor: "#114d9d",
+  },
+  activeTagText: {
+    color: "#114d9d",
+    fontSize: 14,
+    fontWeight: "500",
+  },
+  tagText: {
+    fontSize: 14,
+    fontWeight: "500",
+  },
+
 });
